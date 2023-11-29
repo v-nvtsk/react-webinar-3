05a31305ea67cd8cf3819a9228f57646f889b410
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:47:01 +0300

    refactor addItem to add items to cart

--- src/store.js
+++ src/store.js
@@ -48,13 +48,23 @@ class Store {
   }
 
   /**
-   * Добавление новой записи
+   * Добавление новой записи в корзину
    */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: generateCode(), title: 'Новая запись'}]
-    })
+  addItem(item) {
+    const cartList = { ...this.state.cart.list };
+    cartList[item.code] !== undefined ? cartList[item.code].count += 1 : cartList[item.code] = { ...item, count: 1 };
+    let cartTotal = { ...this.state.cart.total };
+    cartTotal.itemsCount += 1;
+    cartTotal.totalCost += item.price;
+
+    this.setState(
+      {
+        ...this.state,
+        cart: {
+          list: cartList,
+          total: cartTotal
+        }
+      })
   };
 
   /**
