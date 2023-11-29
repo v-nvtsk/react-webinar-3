6c5f30d07e552efd90361023e87282699a4c24bc
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:51:14 +0300

    update Head component

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -2,16 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import './style.css';
 
-function Head({title}) {
+function Head({ title, hasActiveButton, onButtonClick }) {
   return (
     <div className='Head'>
-      <h1>{title}</h1>
+      <h1 className='Head-title'>{title}</h1>
+      {hasActiveButton && <div className="Head-actions">
+        <button className="Head-actions-btn" onClick={() => onButtonClick()}>Закрыть</button>
+      </div>}
     </div>
   )
 }
 
 Head.propTypes = {
-  title: PropTypes.node,
+  title: PropTypes.string,
+  onButtonClick: PropTypes.func,
+  hasActiveButton: PropTypes.bool
 };
+
+Head.defaultProps = {
+  title: "",
+  onButtonClick: () => { },
+  hasActiveButton: false
+}
 
 export default React.memo(Head);
