fdd49ec57b46cda4600e7b93358f578d669a622d
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:56:09 +0300

    remove selection function and state from Item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -5,34 +5,17 @@ import './style.css';
 
 function Item(props) {
 
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
   const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: (e) => {
+    onAction: (e) => {
       e.stopPropagation();
-      props.onDelete(props.item.code);
-
+      props.onAction(props.item);
     }
   }
 
   return (
-    <div className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-         onClick={callbacks.onClick}>
+    <div className='Item'>
       <div className='Item-code'>{props.item.code}</div>
-      <div className='Item-title'>
-        {props.item.title} {count ? ` | Выделяли ${count} ${plural(count, {
-        one: 'раз',
-        few: 'раза',
-        many: 'раз'
-      })}` : ''}
-      </div>
+      <div className='Item-title'>{props.item.title}</div>
       <div className='Item-actions'>
         <button onClick={callbacks.onDelete}>
           Удалить
@@ -46,7 +29,7 @@ Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
+    price: PropTypes.number,
     count: PropTypes.number
   }).isRequired,
   onDelete: PropTypes.func,
@@ -54,10 +37,7 @@ Item.propTypes = {
 };
 
 Item.defaultProps = {
-  onDelete: () => {
-  },
-  onSelect: () => {
-  },
+  onAction: () => { }
 }
 
 export default React.memo(Item);
