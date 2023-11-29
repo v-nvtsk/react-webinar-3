3036573fd89c6dd084cfe52618e5b07af60ece80
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:58:51 +0300

    add formatMoney function import

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
-import {plural} from "../../utils";
+import { formatMoney } from "../../utils";
 import './style.css';
 
 function Item(props) {
