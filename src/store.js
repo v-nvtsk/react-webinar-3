602de47fa4b86fdc061b1831b0b57080a527645a
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:48:21 +0300

    refactor store
    remove selectItem
    refactor deleteItem to delete item from cart

--- src/store.js
+++ src/store.js
@@ -71,35 +71,22 @@ class Store {
    * Удаление записи по коду
    * @param code
    */
-  deleteItem(code) {
+  deleteItem({ code }) {
+    const cartList = { ...this.state.cart.list };
+    const cartTotal = { ...this.state.cart.total };
+    cartTotal.itemsCount -= cartList[code].count;
+    cartTotal.totalCost -= cartList[code].count * cartList[code].price;
+    delete cartList[code]
+
     this.setState({
       ...this.state,
       // Новый список, в котором не будет удаляемой записи
-      list: this.state.list.filter(item => item.code !== code)
+      cart: {
+        list: cartList,
+        total: cartTotal
+      }
     })
   };
-
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          // Смена выделения и подсчёт
-          return {
-            ...item,
-            selected: !item.selected,
-            count: item.selected ? item.count : item.count + 1 || 1,
-          };
-        }
-        // Сброс выделения если выделена
-        return item.selected ? {...item, selected: false} : item;
-      })
-    })
-  }
 }
 
 export default Store;
