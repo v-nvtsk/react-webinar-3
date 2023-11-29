54b94383da8f15eb37055b9925cecf72f749defd
v-nvtsk <v@nvtsk.ru>
2023-11-29 21:27:20 +0300

    add Cart information to Controls

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -4,9 +4,18 @@ import './style.css';
 import { plural, formatMoney } from "../../utils";
 
 function Controls({ onShowCart, cartTotal }) {
+
+  const pluralItems = plural(cartTotal.itemsCount, { one: 'товар', few: 'товара', many: 'товаров' })
+  const cartMessage = `${cartTotal.itemsCount} ${pluralItems} / ${formatMoney(cartTotal.totalCost)}`
+  const emptyCartMessage = 'пусто'
+
   return (
     <div className='Controls'>
-      <button onClick={() => onAdd()}>Добавить</button>
+      <div className="Controls-cart">В корзине:
+        <span className="Controls-cartInfo">
+          {cartTotal.itemsCount > 0 ? cartMessage : emptyCartMessage}
+        </span>
+      </div>
     </div>
   )
 }
