0ac80fb1a7a6509a5d00a51cde685379c1c7c86b
v-nvtsk <v@nvtsk.ru>
2023-11-30 11:35:17 +0300

    add Cart component empty cart message

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -11,7 +11,8 @@ export default function Cart({ cart, onCloseButtonClick, onDeleteItem }) {
     <div className='Cart'>
       <Head title='Корзина' hasActiveButton={true} onButtonClick={onCloseButtonClick} />
       <div className="Cart-body">
-        <List list={Object.values(cart.list)} actionType='delete' onAction={onDeleteItem} />
+        {cart.total.itemsCount > 0 && <List list={Object.values(cart.list)} actionType='delete' onAction={onDeleteItem} />}
+        {cart.total.itemsCount === 0 && <p className="Cart-is-empty">Пусто</p>}
       </div>
       <div className="Cart-footer">
         <p className="Cart-total-text">Итого</p>
