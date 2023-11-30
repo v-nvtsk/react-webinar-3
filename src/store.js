09d4043a3227b9530de753d9fe513d225c0d311b
v-nvtsk <v@nvtsk.ru>
2023-11-30 11:47:07 +0300

    fix total items count unique items in Cart list

--- src/store.js
+++ src/store.js
@@ -53,7 +53,7 @@ class Store {
     const cartList = { ...this.state.cart.list };
     cartList[item.code] !== undefined ? cartList[item.code].count += 1 : cartList[item.code] = { ...item, count: 1 };
     let cartTotal = { ...this.state.cart.total };
-    cartTotal.itemsCount += 1;
+    cartTotal.itemsCount = Object.keys(cartList).length;
     cartTotal.totalCost += item.price;
 
     this.setState(
@@ -73,7 +73,7 @@ class Store {
   deleteItem({ code }) {
     const cartList = { ...this.state.cart.list };
     const cartTotal = { ...this.state.cart.total };
-    cartTotal.itemsCount -= cartList[code].count;
+    cartTotal.itemsCount -= 1;
     cartTotal.totalCost -= cartList[code].count * cartList[code].price;
     delete cartList[code]
 
